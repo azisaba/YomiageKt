efd5a4eccc46ea884b51c6e8b391ab59b98538c3
sysnote8main <sysnote8main@gmail.com>
2025-11-28 20:38:07 +0900

    feat: add AudioPlayerSendHandler temporary

---
+++ src/main/kotlin/net/azisaba/yomiagekt/audio/AudioPlayerSendHandler.kt
@@ -0,0 +1,20 @@
+package net.azisaba.yomiagekt.audio
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame
+import net.dv8tion.jda.api.audio.AudioSendHandler
+import java.nio.ByteBuffer
+
+class AudioPlayerSendHandler(
+    private val audioPlayer: AudioPlayer,
+    private var lastFrame: AudioFrame?,
+) : AudioSendHandler {
+    override fun canProvide(): Boolean {
+        lastFrame = audioPlayer.provide()
+        return lastFrame != null
+    }
+
+    override fun provide20MsAudio(): ByteBuffer? = ByteBuffer.wrap(lastFrame?.data)
+
+    override fun isOpus(): Boolean = true
+}
